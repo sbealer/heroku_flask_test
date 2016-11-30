f9c14afa5ed6bf42a4a4d7d379135783b76a9c35
steve.bealer <stevebealer@gmail.com>
2016-11-29 17:32:36 -0800

    added some selenium testing

--- main_script.py
+++ main_script.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import os
+from selenium import webdriver
+
 #import google_test
 app = Flask(__name__)
 
@@ -10,5 +12,23 @@ def hello_world():
 @app.route('/auth')
 def auth():
     return 'The username is: ' + os.environ['AZN_AUTH1']
+
+@app.route('/selenium')
+def test():
+    driver = webdriver.Chrome()
+    driver.get("http://www.youtube.com/results?search_query=" + "guitar+lessons")
+
+    results = driver.find_elements_by_xpath('//div[@class="yt-lockup-content"]')
+
+    print(len(results))
+
+    for result in results:
+        video = result.find_element_by_xpath('.//h3/a')
+        title = video.get_attribute('title')
+        url = video.get_attribute('href')
+        print("{} ({})".format(title, url))
+    driver.quit()
+
+
 if __name__ == '__main__':
     app.run()
